ce847409b951d758d8c4d6061db0aa1cdff44ae2
leejuyeon96 <133620354+leejuyeon96@users.noreply.github.com>
2023-10-12 19:18:15 +0900

    home.js

--- shallwejeju/src/pages/Home.js
+++ shallwejeju/src/pages/Home.js
@@ -3,40 +3,27 @@ import styled from "styled-components";
 // import { FontAwesomeIcon } from "@fontawesome/react-fontawesome";
 import React, { useRef } from "react";
 
-const StyledAside = styled.aside`
-  display: none;
-  position: fixed;
-  background-color: white;
-  width: 70px;
-  overflow-y: auto;
-  height: 100%;
-
-  &::-webkit-scrollbar {
-    width: 10px;
-  }
-  &::-webkit-scrollbar-thumb {
-    background-color: #999;
-    border-radius: 10px;
-  }
-  &::-webkit-scrollbar-track {
-    background-color: white;
-  }
-`;
-const StyledHome = styled.div`
+const StyledMain = styled.main`
   video {
-    border-radius: 15px;
-    height: 220px;
+    width: 100%;
+    height: 100%;
     object-fit: cover;
+  }
+  logo {
+    
   }
 `;
 
 const Home = () => {
-  const videoRef = useRef(null);
   return (
-    <StyledHome>
-      <video ref={videoRef} src="../assets/sea_-_10816(720p).mp4"></video>
-    </StyledHome>
+    <StyledMain>
+      <video muted autoPlay loop>
+        <source src="/videos/KakaoTalk_20231012_191144896.mp4" type="video/mp4"></source>
+      </video>
+      <logo
+    </StyledMain>
   );
 };
+
 
 export default Home;
