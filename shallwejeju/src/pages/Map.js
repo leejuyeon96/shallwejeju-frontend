4d9253715e6ad3872358b2737fe6c9a75ec272d2
leejuyeon96 <133620354+leejuyeon96@users.noreply.github.com>
2023-10-19 12:53:10 +0900

    Map.js

--- shallwejeju/src/pages/Map.js
+++ shallwejeju/src/pages/Map.js
@@ -1,30 +1,29 @@
 import { useEffect, useRef } from "react";
-
+const { kakao } = window;
 
 const KakaoMap = () => {
+  var container = document.getElementById("map");
+  const mapRef = useRef(null); // hook (참조)
+  var options = {
+    center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도 중심 좌표
+    level: 9,
+  };
 
-    // var container = document.getElementById('map');
-    const mapRef = useRef(null); // hook (참조)
-		var options = {
-			center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도 중심 좌표
-			level: 3
-		};
+  // 마커가 표시될 위치입니다
+  var markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
 
-    // 마커가 표시될 위치입니다 
-   // var markerPosition  = new window.kakao.maps.LatLng(33.450701, 126.570667); 
+  // 마커를 생성합니다
+  var marker = new window.kakao.maps.Marker({
+    position: markerPosition,
+  });
 
-    // 마커를 생성합니다
-  //  var marker = new window.kakao.maps.Marker({
-  //      position: markerPosition
-  //  });
+  useEffect(() => {
+    var map = new window.kakao.maps.Map(mapRef.current, options);
+    //마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+  }, []);
 
-   // useEffect(() => {
-  //      var map = new window.kakao.maps.Map(mapRef.current, options);
-        // 마커가 지도 위에 표시되도록 설정합니다
-  //      marker.setMap(map);
- //   }, [])
-	
-    return <div ref={mapRef} style={{width: "100%", height:"100vh"}}></div>;
-}
+  return <div ref={mapRef} style={{ width: "100%", height: "100vh" }}></div>;
+};
 
 export default KakaoMap;
--- shallwejeju/src/pages/MyPage.js
+++ shallwejeju/src/pages/MyPage.js
@@ -3,13 +3,12 @@ import "../fonts/font.css";
 // import "../resources/profileImg.jpg";
 import React from "react";
 
-
 const StyleMain = styled.main`
   .text {
     margin-top: 28px;
     background-color: #dcdcdc;
     height: 80px;
-  
+
     p {
       height: 50px;
       line-height: 70px;
@@ -21,7 +20,7 @@ const StyleMain = styled.main`
     }
   }
   .profileImg {
-      border-radius: 50%;
+    border-radius: 50%;
   }
 `;
 const MyPage = () => {
@@ -30,10 +29,8 @@ const MyPage = () => {
       <div className="text">
         <p>마이페이지</p>
       </div>
-      <div className="profile-photo">
-      
-      </div>
-      <div className="profile-name"></div>
+      {/* <div className="profile-photo">{img}</div>
+      <div className="profile-name">{name}</div> */}
     </StyleMain>
   );
 };
